4bf694458f315ef369505d167c5230ddfbad13cf
registaalfe <alfexcrstds07@gmail.com>
2025-05-10 11:26:18 +0700

    feat: added icon in hero page

---
+++ src/components/main/hero/HeroSection.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Row, Col } from 'reactstrap';
+
+const HeroSection = () => {
+    return (
+        <div className="jumbotron mb-4 py-3 py-sm-5">
+            <section className="d-flex px-4 w-100 justify-content-center">
+                <div>
+                    <Container>
+                        <Row>
+                            <Col xs="12" className="d-flex">
+                                <div className="icon d-flex flex-row py-5">
+                                    <img src="./img/icon.png" className="img-icon" alt="icon" />
+                                    <h2 className="px-2 text-white">
+                                        Hitask
+                                    </h2>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default HeroSection;
